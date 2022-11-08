668c46e32526613e77258c033f482ad2a6e362f0
David Tassama <davidtassama464@gmail.com>
2022-11-08 13:17:14 +0200

    dji-link-project

--- pages/dji.jsx
+++ pages/dji.jsx
@@ -42,7 +42,7 @@ const DjiStore = () => {
             <button className="px-8 py-2 mt-4 mr-8">Code</button>
           </a>
           <a
-            href="https://dji-store.netlify.app/"
+            href="https://dji-store.netlify.app"
             target="_blank"
             rel="noreferrer"
           >
